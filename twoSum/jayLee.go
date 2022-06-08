c44012a2033f3d84e00888da17dcd756568d627d
jaylee <jaylee630@163.com>
2022-06-08 23:08:54 +0800

    feat: 两数之和

---
+++ twoSum/jayLee.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	h := map[int]int{}
+	for i := range nums {
+		if _, ok := h[target-nums[i]]; ok {
+			return []int{h[target-nums[i]], i}
+		}
+		h[nums[i]] = i
+	}
+	return []int{}
+}
