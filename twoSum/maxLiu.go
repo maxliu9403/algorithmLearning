cae6dd5aa471f14e410b5e868afb3a5f7894d6ac
liuxiang <max.liu@ucloud.cn>
2022-06-08 21:47:37 +0800

    feat: twoSum first commit

--- twoSum/maxLiu.go
+++ twoSum/maxLiu.go
@@ -1,1 +1,20 @@
 package twoSum
+
+// 时间复杂度O(n)
+// 空间复杂度O(n)
+func TwoSum(nums []int, target int) []int {
+	ans := make([]int, 0)
+	tmp := make(map[int]int, 0)
+	for k, v := range nums {
+		tmp[v] = k
+	}
+	for k, v := range nums {
+		gap := target - v
+		index, ok := tmp[gap]
+		if ok && index != k {
+			ans = []int{k, index}
+			return ans
+		}
+	}
+	return ans
+}
