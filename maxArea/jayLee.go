6d3547611991eb2e9ef44715701216938b99ce9c
李浩洁 <jay.lee@ucloud.cn>
2022-06-10 23:18:14 +0800

    feat: max area

--- maxArea/jayLee.go
+++ maxArea/jayLee.go
@@ -1,1 +1,23 @@
 package maxArea
+
+func maxArea(height []int) int {
+	left, right, maxS := 0, len(height)-1, 0
+	for left < right {
+		if s := (right - left) * min(height[left], height[right]); maxS < s {
+			maxS = s
+		}
+		if height[left] <= height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return maxS
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
