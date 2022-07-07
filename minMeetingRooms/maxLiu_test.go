b6c75e601abbbdf9d029029cfb88fb39bb9b2344
liuxiang <max.liu@ucloud.cn>
2022-07-07 09:23:04 +0800

    feat: 会议室1、2

---
+++ minMeetingRooms/maxLiu_test.go
@@ -0,0 +1,21 @@
+package minMeetingRooms
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMinMeetingRoomsMaxLiu(t *testing.T) {
+
+	i1 := [][]int{{0, 30}, {5, 10}, {15, 20}}
+	o1 := minMeetingRoomsMaxLiu(i1)
+	fmt.Println("==", o1)
+
+	i2 := [][]int{{7, 10}, {2, 4}}
+	o2 := minMeetingRoomsMaxLiu(i2)
+	fmt.Println("==", o2)
+
+	i3 := [][]int{{1, 10}, {2, 7}, {3, 19}, {8, 12}, {10, 20}, {11, 30}}
+	o3 := minMeetingRoomsMaxLiu(i3)
+	fmt.Println("==", o3)
+}
