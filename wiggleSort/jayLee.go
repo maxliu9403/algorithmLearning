32fe0454e9a62a7e00f8b67de6bac0b41c227d32
李浩洁 <jay.lee@ucloud.cn>
2022-07-11 14:19:31 +0800

    feat: wiggleSort

--- wiggleSort/jayLee.go
+++ wiggleSort/jayLee.go
@@ -1,1 +1,22 @@
 package wiggleSort
+
+import (
+	"sort"
+)
+
+// todo 快选 + 三数切分
+func wiggleSort(nums []int) {
+	numsBak := append([]int{}, nums...)
+	sort.Ints(numsBak)
+	n := len(nums)
+	// 将数组分为2部分，[(n+1)/2]
+	for idx, i, j := 0, (n+1)/2-1, n-1; idx < n; idx++ {
+		if idx%2 == 0 { // i
+			nums[idx] = numsBak[i]
+			i--
+		} else {
+			nums[idx] = numsBak[j]
+			j--
+		}
+	}
+}
--- wiggleSort/wiggleSort_test.go
+++ wiggleSort/wiggleSort_test.go
@@ -1,1 +1,16 @@
 package wiggleSort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWiggleSort(t *testing.T) {
+	nums := []int{1, 3, 2, 2, 3, 1}
+	//nums1 := []int{1, 5, 1, 6, 1, 4}
+	// [1,5,1,1,6,4] -> [1,1,1,4,5,6] ->
+	// 1, 5, 1, 1, 6, 4
+	wiggleSort(nums)
+	// Output: [2,3,1,3,1,2]
+	fmt.Println("output:", nums)
+}
