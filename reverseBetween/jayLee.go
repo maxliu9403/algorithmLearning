a14fedbf6e94919d80f3b63214b2f28bc99775bd
李浩洁 <jay.lee@ucloud.cn>
2022-07-26 00:33:15 +0800

    feat: reverseBetween

---
+++ reverseBetween/jayLee.go
@@ -0,0 +1,54 @@
+package reverseBetween
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表。
+ */
+func reverseLinkedList(head *ListNode) {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+}
+
+func reverseBetween(head *ListNode, left, right int) *ListNode {
+	// 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+	dummyNode := &ListNode{Val: -1, Next: head}
+
+	pre := dummyNode
+	// 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+	// 建议写在 for 循环里，语义清晰
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+
+	// 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+	rightNode := pre
+	for i := 0; i < right-left+1; i++ {
+		rightNode = rightNode.Next
+	}
+
+	// 第 3 步：切断出一个子链表（截取链表）
+	leftNode := pre.Next // 截取：[left, right]
+	curr := rightNode.Next
+
+	// 注意：切断链接
+	pre.Next = nil
+	rightNode.Next = nil
+
+	// 第 4 步：同第 206 题，反转链表的子区间
+	reverseLinkedList(leftNode)
+
+	// 第 5 步：接回到原来的链表中
+	pre.Next = rightNode
+	leftNode.Next = curr
+	return dummyNode.Next
+}
